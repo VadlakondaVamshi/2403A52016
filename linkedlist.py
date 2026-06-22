a4c41e87c860909f055f6d5eb1fa384134ad763a
vamshi <vamshi@LAPTOP-AT6GCIHU.com>
2026-06-22 14:35:30 +0530

    second commit

---
+++ linkedlist.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data):
+        self.data = None
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def add_end(self, data):
+        new = Node(data)
+        if self.head is None:
+            self.head = new
+        itr=self.head
+        while itr.next:
+            itr=itr.next
+        itr.next=new
+        
